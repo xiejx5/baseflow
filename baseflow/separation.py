f9a25165c425c42b0d304816b513b39deddfceb9
Cody James <xiejx5@gmail.com>
2025-03-18 09:59:50 +0100

    align NaN positions in baseflow with runoff

--- baseflow/separation.py
+++ baseflow/separation.py
@@ -5,85 +5,90 @@ from pathlib import Path
 from baseflow.methods import *
 from baseflow.comparision import strict_baseflow, KGE
 from baseflow.utils import clean_streamflow, exist_ice, geo2imagexy, format_method
-from baseflow.param_estimate import recession_coefficient, param_calibrate, maxmium_BFI
+from baseflow.param_estimate import recession_coefficient, param_calibrate
 
 
-def single(series, area=None, ice=None, method='all', return_kge=True):
-    Q, date = clean_streamflow(series)
+def single(series, area=None, ice=None, method="all", return_kge=True):
+    date, Q = clean_streamflow(series)
     method = format_method(method)
 
     # convert ice_period ([11, 1], [3, 31]) to bool array
     if not isinstance(ice, np.ndarray) or ice.shape[0] == 12:
         ice = exist_ice(date, ice)
     strict = strict_baseflow(Q, ice)
-    if any(m in ['Chapman', 'CM', 'Boughton', 'Furey', 'Eckhardt', 'Willems'] for m in method):
+    if any(m in ["Chapman", "CM", "Boughton", "Furey", "Eckhardt", "Willems"] for m in method):
         a = recession_coefficient(Q, strict)
 
     b_LH = LH(Q)
     b = pd.DataFrame(np.nan, index=date, columns=method)
     for m in method:
-        if m == 'UKIH':
+        if m == "UKIH":
             b[m] = UKIH(Q, b_LH)
 
-        if m == 'Local':
+        if m == "Local":
             b[m] = Local(Q, b_LH, area)
 
-        if m == 'Fixed':
+        if m == "Fixed":
             b[m] = Fixed(Q, area)
 
-        if m == 'Slide':
+        if m == "Slide":
             b[m] = Slide(Q, area)
 
-        if m == 'LH':
+        if m == "LH":
             b[m] = b_LH
 
-        if m == 'Chapman':
+        if m == "Chapman":
             b[m] = Chapman(Q, b_LH, a)
 
-        if m == 'CM':
+        if m == "CM":
             b[m] = CM(Q, b_LH, a)
 
-        if m == 'Boughton':
+        if m == "Boughton":
             C = param_calibrate(np.arange(0.0001, 0.1, 0.0001), Boughton, Q, b_LH, a)
             b[m] = Boughton(Q, b_LH, a, C)
 
-        if m == 'Furey':
+        if m == "Furey":
             A = param_calibrate(np.arange(0.01, 10, 0.01), Furey, Q, b_LH, a)
             b[m] = Furey(Q, b_LH, a, A)
 
-        if m == 'Eckhardt':
+        if m == "Eckhardt":
             # BFImax = maxmium_BFI(Q, b_LH, a, date)
             BFImax = param_calibrate(np.arange(0.001, 1, 0.001), Eckhardt, Q, b_LH, a)
             b[m] = Eckhardt(Q, b_LH, a, BFImax)
 
-        if m == 'EWMA':
+        if m == "EWMA":
             e = param_calibrate(np.arange(0.0001, 0.1, 0.0001), EWMA, Q, b_LH, 0)
             b[m] = EWMA(Q, b_LH, 0, e)
 
-        if m == 'Willems':
+        if m == "Willems":
             w = param_calibrate(np.arange(0.001, 1, 0.001), Willems, Q, b_LH, a)
             b[m] = Willems(Q, b_LH, a, w)
 
     if return_kge:
-        KGEs = pd.Series(KGE(b[strict].values, np.repeat(
-            Q[strict], len(method)).reshape(-1, len(method))), index=b.columns)
+        KGEs = pd.Series(
+            KGE(b[strict].values, np.repeat(Q[strict], len(method)).reshape(-1, len(method))),
+            index=b.columns,
+        )
         return b, KGEs
     else:
         return b, None
 
 
-def separation(df, df_sta=None, method='all', return_bfi=False, return_kge=False):
+def separation(df, df_sta=None, method="all", return_bfi=False, return_kge=False):
     # baseflow separation worker for single station
     def sep_work(s):
         try:
             # read area, longitude, latitude from df_sta
             area, ice = None, None
-            to_num = lambda col: (pd.to_numeric(df_sta.loc[s, col], errors='coerce')
-                                  if (df_sta is not None) and (col in df_sta.columns) else np.nan)
-            if np.isfinite(to_num('area')):
-                area = to_num('area')
-            if np.isfinite(to_num('lon')):
-                c, r = geo2imagexy(to_num('lon'), to_num('lat'))
+            to_num = lambda col: (
+                pd.to_numeric(df_sta.loc[s, col], errors="coerce")
+                if (df_sta is not None) and (col in df_sta.columns)
+                else np.nan
+            )
+            if np.isfinite(to_num("area")):
+                area = to_num("area")
+            if np.isfinite(to_num("lon")):
+                c, r = geo2imagexy(to_num("lon"), to_num("lat"))
                 ice = ~thawed[:, r, c]
                 ice = ([11, 1], [3, 31]) if ice.all() else ice
             # separate baseflow for station S
@@ -96,6 +101,7 @@ def separation(df, df_sta=None, method='all', return_bfi=False, return_kge=False
             if return_kge:
                 df_kge.loc[s] = KGEs
         except BaseException:
+            print("\nFailed to separate baseflow for station {sta}".format(sta=s))
             pass
 
     # convert index to datetime
@@ -103,13 +109,12 @@ def separation(df, df_sta=None, method='all', return_bfi=False, return_kge=False
         df.index = pd.to_datetime(df.index)
 
     # thawed months from https://doi.org/10.5194/essd-9-133-2017
-    with np.load(Path(__file__).parent / 'thawed.npz') as f:
-        thawed = f['thawed']
+    with np.load(Path(__file__).parent / "thawed.npz") as f:
+        thawed = f["thawed"]
 
     # create df to store baseflow
     method = format_method(method)
-    dfs = {m: pd.DataFrame(np.nan, index=df.index, columns=df.columns, dtype=float)
-           for m in method}
+    dfs = {m: pd.DataFrame(np.nan, index=df.index, columns=df.columns, dtype=float) for m in method}
 
     # create df to store BFI and KGE
     if return_bfi:
--- baseflow/utils.py
+++ baseflow/utils.py
@@ -6,9 +6,7 @@ def clean_streamflow(series):
     date, Q = series.index, series.values.astype(float)
     has_value = np.isfinite(Q)
     date, Q = date[has_value], np.abs(Q[has_value])
-    year_unique, counts = np.unique(date.year, return_counts=True)
-    keep = np.isin(date.year, year_unique[counts >= 120])
-    return Q[keep], date[keep]
+    return date, Q
 
 
 def exist_ice(date, ice_period):
@@ -20,19 +18,23 @@ def exist_ice(date, ice_period):
 
     beg, end = ice_period
     if (end[0] > beg[0]) or ((end[0] == beg[0]) & (end[1] > beg[1])):
-        ice = (((date.month > beg[0]) & (date.month < end[0])) |
-               ((date.month == beg[0]) & (date.day >= beg[1])) |
-               ((date.month == end[0]) & (date.day <= end[1])))
+        ice = (
+            ((date.month > beg[0]) & (date.month < end[0]))
+            | ((date.month == beg[0]) & (date.day >= beg[1]))
+            | ((date.month == end[0]) & (date.day <= end[1]))
+        )
     else:
-        ice = (((date.month > beg[0]) | (date.month < end[0])) |
-               ((date.month == beg[0]) & (date.day >= beg[1])) |
-               ((date.month == end[0]) & (date.day <= end[1])))
+        ice = (
+            ((date.month > beg[0]) | (date.month < end[0]))
+            | ((date.month == beg[0]) & (date.day >= beg[1]))
+            | ((date.month == end[0]) & (date.day <= end[1]))
+        )
     return ice
 
 
 def moving_average(x, w):
     res = np.convolve(x, np.ones(w)) / w
-    return res[w - 1:-w + 1]
+    return res[w - 1 : -w + 1]
 
 
 @njit
@@ -42,7 +44,7 @@ def multi_arange(starts, stops):
     res = np.zeros((cnt,), dtype=np.int64)
     for i in range(starts.size):
         num = stops[i] - starts[i]
-        res[pos:pos + num] = np.arange(starts[i], stops[i])
+        res[pos : pos + num] = np.arange(starts[i], stops[i])
         pos += num
     return res
 
@@ -55,9 +57,21 @@ def geo2imagexy(x, y):
 
 
 def format_method(method):
-    if method == 'all':
-        method = ['UKIH', 'Local', 'Fixed', 'Slide', 'LH', 'Chapman',
-                  'CM', 'Boughton', 'Furey', 'Eckhardt', 'EWMA', 'Willems']
+    if method == "all":
+        method = [
+            "UKIH",
+            "Local",
+            "Fixed",
+            "Slide",
+            "LH",
+            "Chapman",
+            "CM",
+            "Boughton",
+            "Furey",
+            "Eckhardt",
+            "EWMA",
+            "Willems",
+        ]
     elif isinstance(method, str):
         method = [method]
     return method
